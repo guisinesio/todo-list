686a89c38325824e454fab650d91aad081913a8a
guiga <f.guilhermeferreirafs@gmail.com>
2023-09-20 00:46:44 -0300

    Remove comentario linha 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-// Seleção de elementos
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
